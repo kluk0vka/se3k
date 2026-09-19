fc2f0eeccdfc31e7011951ae1da636925cd4adc7
leonteva.ekaterina <katya_leont@mail.ru>
2026-09-19 04:32:20 +0300

    Add x-served-by response header to all services

--- services/common/staybook_common/app.py
+++ services/common/staybook_common/app.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+import socket
+
+from fastapi import FastAPI, Request
 from prometheus_fastapi_instrumentator import Instrumentator
+
+HOSTNAME = socket.gethostname()
 
 
 def instrument(app: FastAPI) -> None:
+    @app.middleware("http")
+    async def served_by(request: Request, call_next):
+        response = await call_next(request)
+        response.headers["x-served-by"] = HOSTNAME
+        return response
+
     Instrumentator(
         excluded_handlers=["/healthz", "/readyz", "/metrics"],
         should_group_status_codes=False,
